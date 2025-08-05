1c1d8e89f826e8cb5356d504b5f2a665d1704fd7
Gabriel S. <gabriel.soliz@inacapmail.cl>
2025-08-04 20:32:15 -0400

    Solución de tipado a la hora de importar a librería

--- types/react-native-toast-lite.d.ts
+++ types/react-native-toast-lite.d.ts
@@ -1,12 +1,11 @@
+import { ComponentType } from 'react';
 import { ToastProps } from './toastTypes';
-declare module 'react-native-toast-lite' {
-  import { ComponentType } from 'react';
-  export const Toaster: ComponentType;
-  export const toast: {
-    error: (message: string, props?: ToastProps['props']) => void;
-    success: (message: string, props?: ToastProps['props']) => void;
-    info: (message: string, props?: ToastProps['props']) => void;
-    warning: (message: string, props?: ToastProps['props']) => void;
-    loading: (message: string, props?: ToastProps['props']) => void;
-  };
-}
+
+export const Toaster: ComponentType;
+export const toast: {
+  error: (message: string, props?: ToastProps['props']) => void;
+  success: (message: string, props?: ToastProps['props']) => void;
+  info: (message: string, props?: ToastProps['props']) => void;
+  warning: (message: string, props?: ToastProps['props']) => void;
+  loading: (message: string, props?: ToastProps['props']) => void;
+};
